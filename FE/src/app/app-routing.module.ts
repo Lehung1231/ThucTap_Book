6ab5a7e1bf585e241c0ccd22b60171fc7cc2d4c3
Lehung1231 <hungltph22978@fpt.edu.vn>
2023-09-13 19:31:32 +0700

    TB-1-Router-UI-header-footer

--- FE/src/app/app-routing.module.ts
+++ FE/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FooterComponent } from './shared/Client/footer/footer.component';
+import { HeaderComponent } from './shared/Client/header/header.component';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: AppComponent,
+  },
+  {
+    path : 'footer',
+    component : FooterComponent,
+  },
+  {
+    path : 'header',
+    component : HeaderComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
