c185602b8e744dacfbe0ece9468def46a6d7a4d4
Lehung1231 <hungltph22978@fpt.edu.vn>
2023-09-14 19:33:22 +0700

    TB-3-BE-CURD-Category

--- BE/src/app.js
+++ BE/src/app.js
@@ -3,6 +3,8 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
 import routerProduct from "./routes/product.js";
+import routerCategory from "./routes/category.js";
+
 //config
 const app = express();
 const API_DB = process.env.API_DB;
@@ -13,6 +15,7 @@ app.use(cors());
 app.use(express.json());
 // router
 app.use("/api", routerProduct);
+app.use("/api", routerCategory);
 
 
 // database config
